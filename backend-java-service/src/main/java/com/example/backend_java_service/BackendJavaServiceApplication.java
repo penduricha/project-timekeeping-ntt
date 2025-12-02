c84732528593ed8d1af96f6c10334796ad8940af
richahoipen <minhyemen@gmail.com>
2025-12-02 13:48:03 +0700

    Initialized project web.

---
+++ backend-java-service/src/main/java/com/example/backend_java_service/BackendJavaServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_java_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJavaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJavaServiceApplication.class, args);
+	}
+
+}
---
+++ backend-java-service/src/test/java/com/example/backend_java_service/BackendJavaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_java_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
